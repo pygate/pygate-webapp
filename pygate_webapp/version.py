35817f60f904e3cc04f1a6a77e36048ec98b2882
apogiatzis <apoyiatzis@outlook.com>
2020-08-07 01:26:53 +0300

    chore: add MANIFEST

--- pygate_webapp/version.py
+++ pygate_webapp/version.py
@@ -1,5 +1,9 @@
+import os
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+
 try:
-    with open("VERSION") as f:
+    with open(os.path.join(this_dir, "VERSION")) as f:
         version = f.read().strip()
 except Exception:
     version = "0.0.1"
